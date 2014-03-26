7357cc9dd3fb7c5780b305b15395590541d68d1e
unknown <rnaik@rick-laptop.mlerf.local>
2014-03-25 19:42:34 -0500

    Fixed spelling error in comments

--- deck.py
+++ deck.py
@@ -6,7 +6,7 @@ class Deck(object):
 	def __init__(self):
 			self.cards = {'Two':2,'Three':3,'Four':4,'Five':5,'Six':6,'Seven':7,'Eight':8,'Nine':9,'Ten':10,'Jack':10,'Queen':10,'King':10,'Ace':1}
 	
-	#get a random card from the deck, delete it drom the dict, then return it
+	#get a random card from the deck, delete it from the dict, then return it
 	def draw(self):
 			card_key = random.choice(self.cards.keys())
 			card = self.cards[card_key];
